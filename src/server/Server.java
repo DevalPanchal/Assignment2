7ed4d1b93bd208f6a993c115fd67ef9f02bee7b6
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-28 14:52:55 -0400

    Added server files

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,31 @@
 package server;
 
-public class Server {
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private ServerSocket serverSocket = null;
+    private final int PORT;
+
+    private File serverFile = new File("serverFile/");
+    private File clientFile = new File("clientFile/");
+
+    public Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        this.PORT = port;
+    }
+
+    public void handleRequests() throws IOException {
+        System.out.printf("Server is listening to port: %d", this.PORT);
+
+        // creating a thread to handle each of the clients
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            ConnectionHandler handler = new ConnectionHandler(clientSocket);
+            Thread handlerThread = new Thread(handler);
+            handlerThread.start();
+        }
+    }
 }
