53758635119c49ef2c25c02a06c74e51cbe0f14c
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-29 18:11:56 -0400

    Server ClientConnectionHandler error -cannot find file

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,5 +1,25 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Controller {
+    private final int PORT = 8080;
+    private String hostName = "localhost";
+    private Socket socket;
+
+    public void initialize() {
+        try {
+            socket = new Socket(hostName, PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- src/server/ClientConnectionHandler.java
+++ src/server/ClientConnectionHandler.java
@@ -10,24 +10,14 @@ import java.util.StringTokenizer;
 public class ClientConnectionHandler implements Runnable {
 
     private Socket socket = null;
-//    private SocketAddress socketAddress;
     private File FILE_TO_SEND = new File("ServerFiles/");
-//    private File FILE_TO_GET = new File("ClientFiles/");
-//
-//    private BufferedReader bufferedReader = null;
-//    private FileInputStream requestInput = null;
-//    private PrintWriter outputStream = null;
     private FileInputStream requestInput = null;
     private BufferedInputStream bufferInputStream = null;
     private OutputStream outputStream = null;
 
-//    private BufferedReader requestInput = null;
-//    private DataOutputStream responseOutput = null;
 
     public ClientConnectionHandler(Socket socket) throws IOException {
         this.socket = socket;
-//        this.socketAddress = socket.getLocalSocketAddress();
-//        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.requestInput = new FileInputStream(String.valueOf(socket.getInputStream()));
     }
 
@@ -57,16 +47,6 @@ public class ClientConnectionHandler implements Runnable {
             }
         }
     }
-
-//    public void sendFile(File baseDir) throws IOException {
-//        DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-//        String fileName = inputStream.readUTF();
-//
-//        OutputStream oS = new FileOutputStream((baseDir + "/" + fileName));
-//        byte[] content = new byte[(int)fileName.length()];
-//
-//
-//    }
 
     private void handleRequests() throws IOException {
         try {
